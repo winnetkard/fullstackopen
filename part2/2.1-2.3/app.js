42a74433d99627e18f019054faab6eda5dc2abec
winnetkard <84256024+winnetkard@users.noreply.github.com>
2021-08-16 16:19:48 -0500

    Create app.js

---
+++ part2/2.1-2.3/app.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Header from './components/Header'
+import Content from './components/Content'
+
+
+const Course = ({ course }) => {
+  return (
+    <div>
+    <Header header = {course.name} />
+    <Content content = {course.parts} />
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 22,
+        id: 4
+      }
+    ]
+  }
+  return <Course course={course} />
+}
+
+export default App
